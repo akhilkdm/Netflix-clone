15e0cd27e7571d43d2f2b7fcbe92ef9551a14ca6
Akhilkdm <akhilkodavalam@gmail.com>
2022-06-14 19:32:41 +0530

    netflix clone

---
+++ src/Components/RowPost/RowPost.js
@@ -0,0 +1,56 @@
+import React,{useEffect,useState} from 'react'
+import Youtube from 'react-youtube'
+import './RowPost.css'
+import axios from '../../axios'
+import {imageUrl,API_KEY} from '../../Constants/constants'
+
+function RowPost(props) {
+  const [movies, setMovies] = useState([])
+  const [urlId,setUrlId]=useState('')
+  useEffect(() => {
+    axios.get(props.url).then(response=>{
+      console.log(response.data)
+      setMovies(response.data.results)
+    }).catch(err=>{
+      // alert('Network Error')
+    })
+  }, [])
+  
+  const opts = {
+    height: '390',
+    width: '100%  ',
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+
+    const handleMovie = (id)=>{
+      console.log(id);
+      axios.get(`/movie/${id}/videos?api_key=${API_KEY}&language=en-US`).then(response=>{
+        if(response.data.results.length!==0){
+          setUrlId(response.data.results[0])
+        }else{
+          console.log('Array Empty');
+        }
+      })
+    }
+
+  return (
+    <div className='row'>
+        <h2>{props.title}</h2>
+        <div className="posters">
+
+          {movies.map((obj)=>
+            <img onClick={()=>handleMovie(obj.id)} className={props.isSmall ? 'smallPoster' : 'poster'} src={`${imageUrl+obj.backdrop_path}`} />
+          )}
+            
+            
+        </div>
+
+     { urlId &&    <Youtube opts={opts} videoId={urlId.key} />  }
+    </div>
+  )
+}
+
+export default RowPost
---
+++ src/Constants/constants.js
@@ -0,0 +1,3 @@
+export const baseUrl='https://api.themoviedb.org/3';
+export const API_KEY='db7ddd2dc217cd075f6b156a0b211193';
+export const imageUrl='https://image.tmdb.org/t/p/original';
